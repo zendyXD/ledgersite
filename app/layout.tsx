f7357374d25b3b4b2d871b4e9cdf01c632d3fe3d
Zendy <zendyofficial2021@gmail.com>
2026-07-05 16:00:29 +0530

    fix: activate dark theme on root html layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="h-screen flex overflow-hidden theme-page">
         <Sidebar />
